478485d6aba6764edb5c011a9efeb429ac5cb6e7
Michael Reile <stimmungswecker@googlemail.com>
2017-05-23 13:37:49 +0200

    token class

---
+++ src/main/java/edu/hm/cs/swa/model/Token.java
@@ -0,0 +1,47 @@
+package edu.hm.cs.swa.model;
+
+/**
+ * Created by CaptainEinsicht on 23.05.2017.
+ * Alle Rechte vorbehalten bei Missbrauch wird zurück Missbraucht
+ */
+public class Token {
+
+    private final String tokenstr;
+
+
+    /**
+     * constructor for class token.
+     *
+     * @param tokenstr the tokenstring.
+     */
+    public Token(final String tokenstr) {
+        this.tokenstr = tokenstr;
+    }
+
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Token)) return false;
+
+        final Token token = (Token) o;
+
+        return getTokenstr().equals(token.getTokenstr());
+    }
+
+
+    @Override
+    public int hashCode() {
+        return getTokenstr().hashCode();
+    }
+
+
+    /**
+     * getter.
+     *
+     * @return tokenstr.
+     */
+    public String getTokenstr() {
+        return tokenstr;
+    }
+}
